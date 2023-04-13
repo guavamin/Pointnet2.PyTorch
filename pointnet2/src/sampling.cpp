24585ee22126c4068465df4fde4407614b584afa
guavamin <78477521+guavamin@users.noreply.github.com>
2023-04-13 02:17:10 -0400

    Update sampling.cpp

--- pointnet2/src/sampling.cpp
+++ pointnet2/src/sampling.cpp
@@ -1,11 +1,13 @@
 #include <torch/serialize/tensor.h>
 #include <ATen/cuda/CUDAContext.h>
 #include <vector>
-#include <THC/THC.h>
+#include <ATen/cuda/CUDAEvent.h>
+
 
 #include "sampling_gpu.h"
 
-extern THCState *state;
+cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+// extern THCState *state;
 
 
 int gather_points_wrapper_fast(int b, int c, int n, int npoints, 
@@ -14,7 +16,7 @@ int gather_points_wrapper_fast(int b, int c, int n, int npoints,
     const int *idx = idx_tensor.data<int>();
     float *out = out_tensor.data<float>();
 
-    cudaStream_t stream = THCState_getCurrentStream(state);
+//     cudaStream_t stream = THCState_getCurrentStream(state);
     gather_points_kernel_launcher_fast(b, c, n, npoints, points, idx, out, stream);
     return 1;
 }
@@ -27,7 +29,7 @@ int gather_points_grad_wrapper_fast(int b, int c, int n, int npoints,
     const int *idx = idx_tensor.data<int>();
     float *grad_points = grad_points_tensor.data<float>();
 
-    cudaStream_t stream = THCState_getCurrentStream(state);
+//     cudaStream_t stream = THCState_getCurrentStream(state);
     gather_points_grad_kernel_launcher_fast(b, c, n, npoints, grad_out, idx, grad_points, stream);
     return 1;
 }
@@ -40,7 +42,7 @@ int furthest_point_sampling_wrapper(int b, int n, int m,
     float *temp = temp_tensor.data<float>();
     int *idx = idx_tensor.data<int>();
 
-    cudaStream_t stream = THCState_getCurrentStream(state);
+//     cudaStream_t stream = THCState_getCurrentStream(state);
     furthest_point_sampling_kernel_launcher(b, n, m, points, temp, idx, stream);
     return 1;
 }
